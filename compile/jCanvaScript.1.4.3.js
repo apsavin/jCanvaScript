4e92bde6490a74452c68005b6a639a36877d1b73
apsavin <box.for.postman@gmail.com>
2011-07-14 10:25:00 +0400

    some bugs fixed

--- compile/jCanvaScript.1.4.3.js
+++ compile/jCanvaScript.1.4.3.js
@@ -10,7 +10,7 @@ var canvases = [],pi=Math.PI*2,
 lastCanvas=0,lastLayer=0,
 underMouse = false,
 regHasLetters = /[A-z]+?/,
-regNumsWithMeasure = /\d.\w\w/
+regNumsWithMeasure = /\d.\w\w/,
 FireFox=window.navigator.userAgent.match(/Firefox\/\w+\.\w+/i),
 radian=180/Math.PI;
 if (FireFox!="" && FireFox!==null)FireFox=true;
@@ -257,6 +257,7 @@ function animateTransforms(key,object,queue)
 function keyEvent(e,key,optns)
 {
 	e=e||window.event;
+	optns[key].event=e;
 	optns[key].code=e.charCode||e.keyCode;
 	optns[key].val=true;
 	optns.redraw=1;
@@ -269,6 +270,7 @@ function mouseEvent(e,key,optns)
 		pageX:e.pageX||e.clientX,
 		pageY:e.pageY||e.clientY
 	};
+	optns[key].event=e;
 	optns[key].x=point.pageX - optns.x;
 	optns[key].y=point.pageY - optns.y;
 	if(optns[key].val)optns.redraw=1;
@@ -2873,6 +2875,7 @@ jCanvaScript.canvas = function(idCanvas)
 		if(optns.mousemove.x!=false)
 		{
 			var point = this.optns.point;
+			point.event=optns.mousemove.event;
 			if(optns.mousemove.object!=false)
 			{
 				var mousemoveObject=optns.mousemove.object;
@@ -2915,7 +2918,7 @@ jCanvaScript.canvas = function(idCanvas)
 			var mouseDownObjects=[mouseDown.object,objectLayer(mouseDown.object)];
 			for(i=0;i<2;i++)
 			{
-				if(typeof mouseDownObjects[i].onmousedown=='function')mouseDownObjects[i].onmousedown({x:mouseDown.x,y:mouseDown.y});
+				if(typeof mouseDownObjects[i].onmousedown=='function')mouseDownObjects[i].onmousedown({x:mouseDown.x,y:mouseDown.y,event:mouseDown.event});
 				if(mouseDownObjects[i].optns.drag.val==true)
 				{
 					var drag=optns.drag;
@@ -2947,7 +2950,7 @@ jCanvaScript.canvas = function(idCanvas)
 			drag=optns.drag;
 			for(i=0;i<2;i++)
 			{
-				if(typeof mouseUpObjects[i].onmouseup=='function')mouseUpObjects[i].onmouseup({x:mouseUp.x,y:mouseUp.y});
+				if(typeof mouseUpObjects[i].onmouseup=='function')mouseUpObjects[i].onmouseup({x:mouseUp.x,y:mouseUp.y,event:mouseUp.event});
 				if(mouseUpObjects[i].optns.drop.val==true && optns.drag.init!==undefined)
 				{
 					if(drag.init==drag.object)
@@ -2977,7 +2980,7 @@ jCanvaScript.canvas = function(idCanvas)
 			for(i=0;i<2;i++)
 			{
 				if(typeof mouseClickObjects[i].onclick == 'function')
-					mouseClickObjects[i].onclick({x:mouseClick.x,y:mouseClick.y});
+					mouseClickObjects[i].onclick({x:mouseClick.x,y:mouseClick.y,event:mouseClick.event});
 			}
 			mouseClick.object=false;
 		}
@@ -2988,7 +2991,7 @@ jCanvaScript.canvas = function(idCanvas)
             for(i=0;i<2;i++)
             {
                 if(typeof mouseDblClickObjects[i].ondblclick == 'function')
-                    mouseDblClickObjects[i].ondblclick({x:mouseDblClick.x,y:mouseDblClick.y});
+                    mouseDblClickObjects[i].ondblclick({x:mouseDblClick.x,y:mouseDblClick.y, event:mouseDblClick.event});
             }
             mouseDblClick.object=false;
         }
