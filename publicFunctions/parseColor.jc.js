0aba9a417f7c7dab305eb34aaef2878b568c8195
Alexander Savin <box.for.postman@gmail.com>
2012-01-10 18:41:13 +0400

    short colors added

--- publicFunctions/parseColor.jc.js
+++ publicFunctions/parseColor.jc.js
@@ -1,59 +1,60 @@
-jCanvaScript.parseColor = function(color)
-{
-	var colorKeeper={
-		color:{
-			val:color,
-			notColor:undefined
-		},
-		r:0,
-		g:0,
-		b:0,
-		a:1};
-	if(color.id!==undefined)
-	{
-		colorKeeper.color.notColor = {
+jCanvaScript.parseColor = function (color) {
+    var colorKeeper = {
+        color:{
+            val:color,
+            notColor:undefined
+        },
+        r:0,
+        g:0,
+        b:0,
+        a:1};
+    if (color.id !== undefined) {
+        colorKeeper.color.notColor = {
             level:color._level,
             canvas:color.optns.canvas.number,
             layer:color.optns.layer.number
         };
-		return colorKeeper;
-	}
-	if(color.r!==undefined)
-	{
-		colorKeeper=checkDefaults(color,{r:0,g:0,b:0,a:1});
-		colorKeeper.color = {
+        return colorKeeper;
+    }
+    if (color.r !== undefined) {
+        colorKeeper = checkDefaults(color, {r:0, g:0, b:0, a:1});
+        colorKeeper.color = {
             val:'rgba(' + colorKeeper.r + ',' + colorKeeper.g + ',' + colorKeeper.b + ',' + colorKeeper.a + ')',
             notColor:undefined
         };
-		return colorKeeper;
-	}
-	if(color.charAt(0)=='#')
-	{
-		colorKeeper.r=parseInt(color.substr(1,2),16);
-		colorKeeper.g=parseInt(color.substr(3,2),16);
-		colorKeeper.b=parseInt(color.substr(5,2),16);
-	}
-	else
-	{
-		var arr=color.split(',');
-		if(arr.length==4)
-		{
-			var colorR = arr[0].split('(');
-			var alpha = arr[3].split(')');
-			colorKeeper.r=parseInt(colorR[1]);
-			colorKeeper.g=parseInt(arr[1]);
-			colorKeeper.b=parseInt(arr[2]);
-			colorKeeper.a=parseFloat(alpha[0]);
-		}
-		if(arr.length==3)
-		{
-			colorR = arr[0].split('(');
-			var colorB = arr[2].split(')');
-			colorKeeper.r=parseInt(colorR[1]);
-			colorKeeper.g=parseInt(arr[1]);
-			colorKeeper.b=parseInt(colorB[0]);
-		}
-	}
-	colorKeeper.color.notColor = undefined;
-	return colorKeeper;
+        return colorKeeper;
+    }
+    if (color.charAt(0) == '#') {
+        if (color.length > 4) {
+            colorKeeper.r = parseInt(color.substr(1, 2), 16);
+            colorKeeper.g = parseInt(color.substr(3, 2), 16);
+            colorKeeper.b = parseInt(color.substr(5, 2), 16);
+        }
+        else {
+            var r = color.charAt(1), g = color.charAt(2), b = color.charAt(3);
+            colorKeeper.r = parseInt(r + r, 16);
+            colorKeeper.g = parseInt(g + g, 16);
+            colorKeeper.b = parseInt(b + b, 16);
+        }
+    }
+    else {
+        var arr = color.split(',');
+        if (arr.length == 4) {
+            var colorR = arr[0].split('(');
+            var alpha = arr[3].split(')');
+            colorKeeper.r = parseInt(colorR[1]);
+            colorKeeper.g = parseInt(arr[1]);
+            colorKeeper.b = parseInt(arr[2]);
+            colorKeeper.a = parseFloat(alpha[0]);
+        }
+        if (arr.length == 3) {
+            colorR = arr[0].split('(');
+            var colorB = arr[2].split(')');
+            colorKeeper.r = parseInt(colorR[1]);
+            colorKeeper.g = parseInt(arr[1]);
+            colorKeeper.b = parseInt(colorB[0]);
+        }
+    }
+    colorKeeper.color.notColor = undefined;
+    return colorKeeper;
 }
